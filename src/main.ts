0532c8f8126330b5272e1786768d118cee164e2e
leSage1945 <koukodilaexauce@gmail.com>
2026-06-19 12:21:24 +0200

    config: ajustement main.ts pour le déploiement

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Permet à votre futur Angular sur Vercel de requêter ce backend
   app.enableCors();
-
-  const port = process.env.PORT ?? 3000;
 
   console.log('🚀 START BOOTSTRAP');
 
-
+  // Utilise le port de Render en production, ou 3000 en local
+  const port = process.env.PORT ?? 3000;
   await app.listen(port);
 
-  console.log(`🚀 Serveur NestJS démarré sur http://localhost:${port}`);
+  console.log(`🚀 Serveur NestJS démarré avec succès sur le port : ${port}`);
 }
 
 bootstrap();
